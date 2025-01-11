78229a9c80b4fb7276974de53ec55aa76197c112
William <wmoura85@gmail.com>
2025-01-11 01:54:25 -0300

    Projeto criado

---
+++ src/main/java/org/yourcompany/chesssystem/ChessSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.chesssystem;
+
+/**
+ *
+ * @author wmoura
+ */
+public class ChessSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
